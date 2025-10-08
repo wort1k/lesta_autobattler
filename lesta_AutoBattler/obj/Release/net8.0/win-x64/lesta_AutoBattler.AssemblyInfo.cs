364cff7853efc1647f1f1427b8a3f868a9df0121
Stepan Kolesnev <wort1k17@gmail.com>
2025-10-08 18:04:57 +0300

    lesta_AutoBattler.exe is now update

--- lesta_AutoBattler/obj/Release/net8.0/win-x64/lesta_AutoBattler.AssemblyInfo.cs
+++ lesta_AutoBattler/obj/Release/net8.0/win-x64/lesta_AutoBattler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lesta_AutoBattler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d9dd16fc19ead3dde277f353cc660b8596b9d45")]
 [assembly: System.Reflection.AssemblyProductAttribute("lesta_AutoBattler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lesta_AutoBattler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
